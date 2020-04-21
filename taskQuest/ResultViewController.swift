b4835f18627924f2243bdb587ec4a4d83e9f09ea
momota-fukuda <momota-fukuda@united.jp>
2020-04-21 15:38:33 +0900

    [fix]結果画面の初期準備

--- taskQuest/ResultViewController.swift
+++ taskQuest/ResultViewController.swift
@@ -6,23 +6,35 @@
 //  Copyright © 2020 momota-fukuda. All rights reserved.
 //
 
-import UIKit
 import RealmSwift
+import UIKit
 
-class ResultViewController: UIViewController {
+class ResultViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet private var statusView: StatusView!
+    @IBOutlet private var pauseOrResumeButton: ToggleButton!
+    @IBOutlet private var skipOrCompleteButton: ToggleButton!
+    @IBOutlet private var eventTableView: UITableView!
 
-    @IBOutlet private weak var statusView: StatusView!
-    @IBOutlet private weak var pauseOrResumeButton: ToggleButton!
-    @IBOutlet private weak var skipOrCompleteButton: ToggleButton!
-    
-      private let realm = try! Realm()
-    
+    private let realm = try! Realm()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.eventTableView.delegate = self
+        self.eventTableView.dataSource = self
+
         self.pauseOrResumeButton.set(firstStateText: "一時停止", secondStateText: "再開   ")
         self.skipOrCompleteButton.set(firstStateText: "スキップ", secondStateText: "終了")
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "eventCell", for: indexPath)
+
+        return cell
+    }
 }
